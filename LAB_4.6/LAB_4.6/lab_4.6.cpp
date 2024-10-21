e3ad1b755d42757aaa1a6c62f9575b40bc42a29b
harD1kkkk <137618635+harD1kkkk@users.noreply.github.com>
2024-10-21 15:23:50 +0300

    Update lab_4.6.cpp

--- LAB_4.6/LAB_4.6/lab_4.6.cpp
+++ LAB_4.6/LAB_4.6/lab_4.6.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     double result;
-    double n, k;
+    int n, k;
 
     // while 
     result = 0;
@@ -12,7 +12,7 @@ int main() {
     while (n <= 18) {
         k = n;
         while (k <= 20) {
-            result += sqrt(abs(1 - k / n)) / (2 * pow(n, 2) + pow(k, 2));
+            result += sqrt(abs(1 - static_cast<double>(k) / static_cast<double>(n))) / (2 * pow(n, 2) + pow(k, 2));
             k++;
         }
         n++;
@@ -25,7 +25,7 @@ int main() {
     do {
         k = n;
         do {
-            result += sqrt(abs(1 - k / n)) / (2 * pow(n, 2) + pow(k, 2));
+            result += sqrt(abs(1 - static_cast<double>(k) / static_cast<double>(n))) / (2 * pow(n, 2) + pow(k, 2));
             k++;
         } while (k <= 20);
         n++;
@@ -34,18 +34,18 @@ int main() {
 
     // for n++
     result = 0;
-    for (double n = 1; n <= 18; n++) {
-        for (double k = n; k <= 20; k++) {
-            result += sqrt(abs(1 - k / n)) / (2 * pow(n, 2) + pow(k, 2));
+    for (int n = 1; n <= 18; n++) {
+        for (int k = n; k <= 20; k++) {
+            result += sqrt(abs(1 - static_cast<double>(k) / static_cast<double>(n))) / (2 * pow(n, 2) + pow(k, 2));
         }
     }
     cout << "Result with for loop (n++): " << result << endl;
 
     // for n--
     result = 0;
-    for (double n = 18; n >= 1; n--) {
-        for (double k = 20; k >= n; k--) {
-            result += sqrt(abs(1 - k / n)) / (2 * pow(n, 2) + pow(k, 2));
+    for (int n = 18; n >= 1; n--) {
+        for (int k = 20; k >= n; k--) {
+            result += sqrt(abs(1 - static_cast<double>(k) / static_cast<double>(n))) / (2 * pow(n, 2) + pow(k, 2));
         }
     }
     cout << "Result with for loop (n--): " << result << endl;
